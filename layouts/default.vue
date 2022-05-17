63745afd87e0367307bbc9a71628c51c612afe3f
MaxEros <gtxuss@gmail.com>
2022-05-17 22:36:32 +0300

    backup

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,7 +18,7 @@ export default {
 		window.onresize = function () {
 			if (window.outerHeight - window.innerHeight > 100) {
 				console.log('Вы открыли панель разработчика')
-				setTimeout(() => (location.href = '/404'), 1500)
+				setTimeout(() => (location = '/404') && (location.href = '/404'), 1500)
 			}
 		}
 	},
