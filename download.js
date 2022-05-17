236754158cf14e490fd1f7725be8e96622e3c9a6
MaxEros <gtxuss@gmail.com>
2022-05-17 13:28:48 +0300

    backup

--- download.js
+++ download.js
@@ -1,9 +1,12 @@
-//const fetch = require('node-fetch/src/index').
+const fetch = require('node-fetch')
 const prismic = require('@prismicio/client')
 const { apiEndpoint } = require('./sm.json')
 const repoName = apiEndpoint?.split('/')?.[2]?.split('.')?.[0]
 const accessToken = require('dotenv').config().parsed.PRC_CDA_ACCESS_TOKEN
 
-const client = prismic.createClient(repoName, { accessToken })
+const client = prismic.createClient(repoName, { fetch, accessToken })
 
-console.log(client)
+async function download() {
+	const test = await client.getFirst()
+	console.log(test)
+}
