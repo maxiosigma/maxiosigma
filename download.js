e8a369b3ce1e48d8ee4825f70de56d1a071e1d32
MaxEros <gtxuss@gmail.com>
2022-05-17 13:18:46 +0300

    backup

--- download.js
+++ download.js
@@ -1,7 +1,7 @@
-//const fetch = require('node-fetch')
+//const fetch = require('node-fetch/src/index').
 const prismic = require('@prismicio/client')
 const { apiEndpoint } = require('./sm.json')
-const repoName = ''
+const repoName = apiEndpoint?.split('/')?.[2]?.split('.')?.[0]
 const accessToken = require('dotenv').config().parsed.PRC_CDA_ACCESS_TOKEN
 
-console.log(apiEndpoint)
+console.log()
