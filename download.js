370cddc46cb78af5c4db91afc6390f460ea97244
MaxEros <gtxuss@gmail.com>
2022-05-17 14:17:27 +0300

    backup

--- download.js
+++ download.js
@@ -6,20 +6,26 @@ const { apiEndpoint } = require('./sm.json')
 const repoName = apiEndpoint?.split('/')?.[2]?.split('.')?.[0]
 const accessToken = require('dotenv').config().parsed.PRC_CDA_ACCESS_TOKEN
 
-const client = prismic.createClient(repoName, { fetch, accessToken })(async () => {
+const client = prismic.createClient(repoName, { fetch, accessToken })
+
+;(async () => {
 	const api_query_links = (await client?.getAllByType('link'))
 		?.filter((it) => it.data.img.url)
 		?.map((it) => it.data.img.url)
 
-	api_query_links.map((it) => {
+ 	api_query_links.map((it) => {
 		//console.log(it)
 		//aaaaaaaaaa
 
-		const downloader = new Downloader({
-			url: it, //If the file name already exists, a new file with the name 200MB1.zip is created.
-			directory: './images/cdn', //This folder will be created, if it doesn't exist.
-		})
-	})
+		const downloader = new downloader({ url: it, directory: './images/cdn' })
 
-	console.log(downloader)
+		try {
+			await downloader.download(); //Downloader.download() returns a promise.
+			console.log("All done");
+		} catch (error) {
+			//IMPORTANT: Handle a possible error. An error is thrown in case of network errors, or status codes of 400 and above.
+			//Note that if the maxAttempts is set to higher than 1, the error is thrown only if all attempts fail.
+			console.log("Download failed", error);
+		}
+	})
 })
