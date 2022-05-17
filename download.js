dbe80f937550bfbe94ce22441183febc87826a3f
MaxEros <gtxuss@gmail.com>
2022-05-17 14:44:07 +0300

    backup

--- download.js
+++ download.js
@@ -20,14 +20,12 @@ const client = prismic.createClient(repoName, { fetch, accessToken })
 				fileName,
 				directory: './images/cdn',
 				cloneFiles: false,
-				maxAttempts: 3,
-				onError: function (error) {
-					console.log(`Ошибка чтения: ${fileName}`, error)
-				},
+				//maxAttempts: 3,
+				//onError: function (error) {
+				//	console.log(`Ошибка чтения: ${fileName}`, error)
+				//},
 				shouldStop: function (error) {
-					if (e.statusCode && e.statusCode === 404) {
-						return true
-					}
+					return e.statusCode && e.statusCode === 404 ? true : false
 				},
 			})
 
