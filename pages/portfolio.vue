21d38c06b0ad3c3f61c4445db8630206634c56ab
MaxEros <gtxuss@gmail.com>
2022-06-19 10:42:11 +0300

    backup

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -12,6 +12,24 @@
 				<div class="text-center">{{ it.title }}</div>
 				<!--<div class="js-object-log"></div>-->
 				<div class="">{{ properties[it.name] }}</div>
+
+				<div class="demo-content align-center morphing-demo">
+					<svg width="140" height="140" viewBox="0 0 140 140">
+						<g fill="none" fill-rule="evenodd">
+							<g fill="currentColor" fill-opacity=".15" transform="translate(0 6)">
+								<polygon points="70 0 136.574 48.369 111.145 126.631 28.855 126.631 3.426 48.369"></polygon>
+								<polygon points="70 18 119.455 53.931 100.565 112.069 39.435 112.069 20.545 53.931"></polygon>
+								<polygon points="70 34.86 101.727 57.911 89.609 95.209 50.391 95.209 38.273 57.911"></polygon>
+								<polygon points="70 50.898 84.864 61.697 79.186 79.171 60.814 79.171 55.136 61.697"></polygon>
+							</g>
+							<polygon
+								class="polymorph"
+								stroke-width="1"
+								stroke="currentColor"
+								points="70 39.111670180848165 118.68507822465598 59.48007822465599 109.9231395287841 130.96482663016016 59.7442177534401 86.51932981915184 18.537670180848167 59.702530652064056 "></polygon>
+						</g>
+					</svg>
+				</div>
 			</div>
 		</div>
 	</LayoutPage>
@@ -39,18 +57,36 @@ export default {
 	},
 	methods: {
 		setAnimation() {
+			//this.$anime({
+			//	targets: this.properties,
+			//	developer: 100,
+			//	designer: 200,
+			//	entrepreneur: 300,
+			//	easing: 'linear',
+			//	duration: 2500,
+			//	delay: 750,
+			//	round: 1,
+			//	//update: function () {
+			//	//	//JSON.stringify(this.properties)
+			//	//},
+			//})
+
 			this.$anime({
-				targets: this.properties,
-				developer: 100,
-				designer: 200,
-				entrepreneur: 300,
-				easing: 'linear',
-				duration: 2500,
-				delay: 750,
-				round: 1,
-				//update: function () {
-				//	//JSON.stringify(this.properties)
-				//},
+				targets: '.morphing-demo .polymorph',
+				points: [
+					{
+						value: [
+							'70 24 119.574 60.369 100.145 117.631 50.855 101.631 3.426 54.369',
+							'70 41 118.574 59.369 111.145 132.631 60.855 84.631 20.426 60.369',
+						],
+					},
+					{ value: '70 6 119.574 60.369 100.145 117.631 39.855 117.631 55.426 68.369' },
+					{ value: '70 57 136.574 54.369 89.145 100.631 28.855 132.631 38.426 64.369' },
+					{ value: '70 24 119.574 60.369 100.145 117.631 50.855 101.631 3.426 54.369' },
+				],
+				easing: 'easeOutQuad',
+				duration: 2000,
+				loop: true,
 			})
 		},
 	},
