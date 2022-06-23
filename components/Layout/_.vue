b6d6615590c5a974718c9bd9780c13630ddcfb08
MaxEros <gtxuss@gmail.com>
2022-06-23 15:44:33 +0300

    backup

--- components/Layout/_.vue
+++ components/Layout/_.vue
@@ -123,4 +123,20 @@ export default {
 .debug__screens {
 	@apply before: (bg-black shadow-xl text-sm text-white px-2.5 rounded-tl-md opacity-70 py-1 bottom-0 right-0 z-99999 fixed) <sm:before:content-["screen-none"] @sm: before:content-["screen-sm"] @md: before:content-["screen-md"] @lg: before:content-["screen-lg"] @xl: before:content-['screen-xl'] @2xl: before:content-["screen-2xl"] ;
 }
+
+.sp-link-wrapper > .sp-link > span {
+	@apply hidden #{!important};
+}
+
+.sp-link > span {
+	@apply hidden #{!important};
+}
+
+.sp-link-wrapper .sp-link > span {
+	@apply hidden #{!important};
+}
+
+.sp-link span {
+	@apply hidden #{!important};
+}
 </style>
