8fa958227b5d85ad8c3f23c7ddf09258065ba686
MaxEros <gtxuss@gmail.com>
2022-06-24 14:23:29 +0300

    backup

---
+++ pages/confidentiality.vue
@@ -0,0 +1,16 @@
+<template>
+	<LayoutPage></LayoutPage>
+</template>
+
+<script>
+export default {
+	head() {
+		return {
+			title: 'Конфиденциальность',
+		}
+	},
+}
+</script>
+
+<style lang="scss">
+</style>
