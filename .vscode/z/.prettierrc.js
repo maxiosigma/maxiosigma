477e0ceeb282d73e2404593264e29cf43e211fa6
MaxEros <gtxuss@gmail.com>
2022-06-24 14:31:06 +0300

    backup

---
+++ .vscode/z/.prettierrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+	semi: false,
+	tabWidth: 2,
+	useTabs: true,
+	printWidth: 180,
+	singleQuote: true,
+	//proseWrap: 'preserve',
+	//insertSpaces: false,
+
+	//bracketSpacing: true,
+	//bracketSameLine: true,
+	//jsxBracketSameLine: true,
+
+	//htmlWhitespaceSensitivity: 'strict',
+	//vueIndentScriptAndStyle: true,
+
+	//trailingComma: 'es5',
+	//arrowParens: 'always',
+	//embeddedLanguageFormatting: 'auto',
+
+	overrides: [
+		{
+			files: ['*.html', '*.vue'],
+			options: {
+				printWidth: 135,
+			},
+		},
+		{
+			files: ['*.js'],
+			options: {
+				semi: false,
+				printWidth: 100,
+				//proseWrap: 'preserve',
+				//insertSpaces: false,
+				//bracketSpacing: true,
+				//bracketSameLine: true,
+				//jsxBracketSameLine: true,
+			},
+		},
+	],
+}
--- store/index.js
+++ store/index.js
@@ -1,96 +1,99 @@
 export const state = () => ({
-	//scroll: 0,
-	//mainMenu: 0,
-	timeout: 1000,
-	//uploadCdn: false,
-	//animateKinesis: true,
-	//modal: {
-	//	discount: false,
-	//},
-	//slides: [],
-	navbar: [],
-	reffers: []
-})
+  //scroll: 0,
+  //mainMenu: 0,
+  timeout: 1000,
+  //uploadCdn: false,
+  //animateKinesis: true,
+  //modal: {
+  //	discount: false,
+  //},
+  //slides: [],
+  navbar: [],
+  reffers: [],
+});
 
 export const mutations = {
-	uploadStrapi(state, {key, payload}) {
-		state[key] = payload
+  uploadStrapi(state, { key, payload }) {
+    state[key] = payload;
 
-		//console.log(state[key], payload);
-	},
-	setUploadCdn(state) {
-		state.uploadCdn = true
-	},
-	addSlide(state, prop) {
-		state.slides.push(prop)
-	},
-	checkScroll(state, prop) {
-		state.scroll = prop
-	},
-	checkMenu(state, prop) {
-		if (prop) state.mainMenu = prop
-		else {
-			state.mainMenu = state.mainMenu == 0 ? 1 : -state.mainMenu
-			state.mainMenu == 1
-				? (document.body.style.overflow = 'hidden')
-				: state.mainMenu == -1
-				? setTimeout(() => (document.body.style.overflow = 'auto'), 2000)
-				: null
-		}
-	},
-	modalOpen(state, name) {
-		state.modal = Object.assign(state.modal, { [name]: true })
-		document.querySelector('.wrapper').classList.add('overflow-hidden')
-	},
-	modalClose(state, name) {
-		state.modal = Object.assign(state.modal, { [name]: false })
-		document.querySelector('.wrapper').classList.remove('overflow-hidden')
-	},
-	deactiveAnimateKinesis(state) {
-		state.animateKinesis = false
-	},
-}
+    //console.log(state[key], payload);
+  },
+  setUploadCdn(state) {
+    state.uploadCdn = true;
+  },
+  addSlide(state, prop) {
+    state.slides.push(prop);
+  },
+  checkScroll(state, prop) {
+    state.scroll = prop;
+  },
+  checkMenu(state, prop) {
+    if (prop) state.mainMenu = prop;
+    else {
+      state.mainMenu = state.mainMenu == 0 ? 1 : -state.mainMenu;
+      state.mainMenu == 1
+        ? (document.body.style.overflow = "hidden")
+        : state.mainMenu == -1
+        ? setTimeout(() => (document.body.style.overflow = "auto"), 2000)
+        : null;
+    }
+  },
+  modalOpen(state, name) {
+    state.modal = Object.assign(state.modal, { [name]: true });
+    document.querySelector(".wrapper").classList.add("overflow-hidden");
+  },
+  modalClose(state, name) {
+    state.modal = Object.assign(state.modal, { [name]: false });
+    document.querySelector(".wrapper").classList.remove("overflow-hidden");
+  },
+  deactiveAnimateKinesis(state) {
+    state.animateKinesis = false;
+  },
+};
 
 export const actions = {
-	async nuxtServerInit(ctx) {
-		const menu = (
-			await this.$strapi.graphql({
-				query: ctx.state.gql.menu,
-			})
-		)?.menusMenus?.data[0]?.attributes?.items?.data
-			.map((it) => it?.attributes)
-			.map((it) => {
-				return {
-					url: it.url?.split('?')?.[0],
-					title: it.title,
-					order: it.order,
-					target: it.target,
-					parent: it.parent.data?.attributes,
-					...it.url
-						?.split('?')?.[1]
-						?.split('&')
-						?.reduce((s, it) => {
-							s = {
-								...s,
-								[it.split('=')[0]]: it.split('=')[1],
-							}
-							return s
-						}, {}),
-				}
-			})
+  async nuxtServerInit(ctx) {
+    const menu = (
+      await this.$strapi.graphql({
+        query: ctx.state.gql.menu,
+      })
+    )?.menusMenus?.data[0]?.attributes?.items?.data
+      .map((it) => it?.attributes)
+      .map((it) => {
+        return {
+          url: it.url?.split("?")?.[0],
+          title: it.title,
+          order: it.order,
+          target: it.target,
+          parent: it.parent.data?.attributes,
+          ...it.url
+            ?.split("?")?.[1]
+            ?.split("&")
+            ?.reduce((s, it) => {
+              s = {
+                ...s,
+                [it.split("=")[0]]: it.split("=")[1],
+              };
+              return s;
+            }, {}),
+        };
+      })
+      .filter((it) => it?.hidden !== "true");
 
-		ctx.commit('uploadStrapi',  { key: "navbar", payload: menu})
+    ctx.commit("uploadStrapi", { key: "navbar", payload: menu });
 
-		const reffers = (await this.$strapi.graphql({ query: ctx.state.gql.links })).links?.data?.map((it) => it.attributes)
+    const reffers = (
+      await this.$strapi.graphql({ query: ctx.state.gql.links })
+    ).links?.data?.map((it) => it.attributes);
 
-		ctx.commit('uploadStrapi',  { key: "reffers", payload: reffers})
+    ctx.commit("uploadStrapi", { key: "reffers", payload: reffers });
 
-		//console.log(ctx.$strapi)
-		//console.log(ctx)
-		//await dispatch('links/getLinks', { prismic: $prismic })
-	},
-}
+    //console.log(ctx.$strapi)
+    //console.log(ctx)
+    //await dispatch('links/getLinks', { prismic: $prismic })
+  },
+};
 
-export const getters = {}
+export const getters = {};
 
 //npx apollo schema:download --endpoint=http://localhost:1337/graphql schema.json
