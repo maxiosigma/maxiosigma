71c350d9aafd79bd348eade0677a90555e306350
MaxEros <gtxuss@gmail.com>
2022-06-25 17:08:07 +0300

    backup

--- pages/about.vue
+++ pages/about.vue
@@ -1,54 +1,61 @@
 <template>
-	<LayoutPage>
-		<!--<div class="about">
-			<div class="bg-gray-400 bg-opacity-0 h-90vh pt-10">
-				<ItemImg class="ml-auto max-h-full object-contain -mr-1/5" src="about/person_1.png"></ItemImg>
-			</div>
-		</div>-->
-	</LayoutPage>
+  <LayoutPage :bodyStyle="'body-bg about'">
+    <div class="container flex-grow flex mx-auto my-0 items-end about-container">
+      <div class="w-3/5">
+        <ItemImg
+          class="h-full object-cover my-0 w-full justify-end"
+          src="about/face_1.png"
+        ></ItemImg>
+      </div>
+    </div>
+  </LayoutPage>
 </template>
 
 <script>
 export default {
-	head() {
-		return {
-			title: 'Автор',
-		}
-	},
-	data() {
-		return {
-			visual: false,
-			deviceType: this.$ua.deviceType(),
-			items: {
-				titles: [
-					{ t: 'Лидер', cl: 'text-yellow-600' },
-					{},
-					{ t: 'Бизнесмен', cl: 'text-red-600' },
-					{},
-					{ t: 'Филантроп', cl: 'text-blue-600' },
-				],
-				abouts: ['Работаю с командой', 'Фрилансер с 2017 года', 'Поддерживаю бренд'],
-				commerce: ['Оцифровываю бизнесы', 'Помогаю с доходом на конкурентах', 'Приобщаю к умным покупкам'],
-				hobbies: ['Разрабатываю сайты', 'Создаю продающий дизайн', 'Занимаюсь спортом'],
-			},
-		}
-	},
-	mounted() {
-		setTimeout(() => {
-			this.visual = true
-		}, this.$store.state.timeout)
+  head() {
+    return {
+      title: "Автор",
+    }
+  },
+  data() {
+    return {
+      visual: false,
+      deviceType: this.$ua.deviceType(),
+      items: {
+        titles: [
+          { t: "Лидер", cl: "text-yellow-600" },
+          {},
+          { t: "Бизнесмен", cl: "text-red-600" },
+          {},
+          { t: "Филантроп", cl: "text-blue-600" },
+        ],
+        abouts: ["Работаю с командой", "Фрилансер с 2017 года", "Поддерживаю бренд"],
+        commerce: [
+          "Оцифровываю бизнесы",
+          "Помогаю с доходом на конкурентах",
+          "Приобщаю к умным покупкам",
+        ],
+        hobbies: ["Разрабатываю сайты", "Создаю продающий дизайн", "Занимаюсь спортом"],
+      },
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.visual = true
+    }, this.$store.state.timeout)
 
-		setTimeout(() => {
-			this.$refs.promo?.scrollIntoView({ behavior: 'smooth', block: 'start' })
-		}, this.$store.state.timeout - 1 + 90601)
+    setTimeout(() => {
+      this.$refs.promo?.scrollIntoView({ behavior: "smooth", block: "start" })
+    }, this.$store.state.timeout - 1 + 90601)
 
-		this.storageItem('about')
-	},
+    this.storageItem("about")
+  },
 }
 </script>
 
 <style lang="scss">
 .about {
-	@apply flex flex-col -mt-20 z-10 fle relative <lg: (flex-wrap pt-5 pb-10) ;
+  //@apply flex flex-col -mt-20 z-10 relative <lg: (flex-wrap pt-5 pb-10) ;
 }
 </style>
--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -1,24 +1,31 @@
 <template>
-	<LayoutPage>
-		<div class="flex-grow filter drop-shadow-lg lg:(grid grid-cols-3) <lg:(flex flex-col) ">
-			<!-- , { 'elevation-10': i == 2 } ДОБАВИТЬ ТЕНИ ПО БОКАМ -->
-			<!-- 	@click.native.prevent="redirect(it.name)" -->
-			<ItemImgStrapiBg :class="[
-				it.name,
-				'flex-grow flex-center relative flex-col cursor-pointer text-light-200 relative bg-top bg-cover <lg:(bg-contain max-w-1/2 flex-shrink flex-grow-0 min-h-300px)',
-				{ '<lg:(mr-auto z-0)': i == 0 },
-				{ '<lg:(ml-auto -my-30 z-10)': i == 1 },
-				{ '<lg:(mr-auto z-20)': i == 2 },
-			]" v-for="(it, i) in [
-	{ name: 'developer', img: '', title: 'Разработчик' },
-	{ name: 'designer', img: '', title: 'Дизайнер' },
-	{ name: 'entrepreneur', img: '', title: 'Предприниматель' },
-]" :key="i" :src="data.Imgs.data[i].attributes.url" :alt="data.Imgs.data[i].attributes.name">
-				<div class="text-center">{{ it.title }}</div>
-				<!--<div class="js-object-log"></div>-->
-				<!--<div class="">{{ properties[it.name] }}</div>-->
+  <LayoutPage>
+    <div class="flex-grow filter drop-shadow-lg lg:(grid grid-cols-3) <lg:(flex flex-col) ">
+      <!-- , { 'elevation-10': i == 2 } ДОБАВИТЬ ТЕНИ ПО БОКАМ -->
+      <!-- 	@click.native.prevent="redirect(it.name)" -->
+      <!--<ItemImgStrapiBg
+        :class="[
+          it.name,
+          'flex-grow flex-center relative flex-col cursor-pointer text-light-200 relative bg-top bg-cover <lg:(bg-contain max-w-1/2 flex-shrink flex-grow-0 min-h-300px)',
+          { '<lg:(mr-auto z-0)': i == 0 },
+          { '<lg:(ml-auto -my-30 z-10)': i == 1 },
+          { '<lg:(mr-auto z-20)': i == 2 },
+        ]"
+        v-for="(it, i) in [
+          { name: 'developer', img: '', title: 'Разработчик' },
+          { name: 'designer', img: '', title: 'Дизайнер' },
+          { name: 'entrepreneur', img: '', title: 'Предприниматель' },
+        ]"
+        :key="i"
+        :src="data.Imgs.data[i].attributes.url"
+        :alt="data.Imgs.data[i].attributes.name"
+      >
+        <div class="text-center">{{ it.title }}</div>
+      </ItemImgStrapiBg>-->
 
-				<!--<div class="demo-content align-center morphing-demo">
+      <!--<div class="js-object-log"></div>
+				<div class="">{{ properties[it.name] }}</div>
+				<div class="demo-content align-center morphing-demo">
 					<svg width="140" height="140" viewBox="0 0 140 140">
 						<g fill="none" fill-rule="evenodd">
 							<g fill="currentColor" fill-opacity=".15" transform="translate(0 6)">
@@ -35,81 +42,80 @@
 						</g>
 					</svg>
 				</div>-->
-			</ItemImgStrapiBg>
-		</div>
-	</LayoutPage>
+    </div>
+  </LayoutPage>
 </template>
 
 <script>
 export default {
-	head() {
-		return {
-			title: 'Портфолио',
-		}
-	},
-	data() {
-		return {
-			properties: {
-				developer: 0,
-				designer: 0,
-				entrepreneur: 0,
-			},
-		}
-	},
-	async asyncData({ $strapi, store }) {
-		const portfolio = store.state.gql.portfolio
-		const data = (
-			await $strapi.graphql({
-				query: portfolio,
-			})
-		)?.page?.data?.attributes
+  head() {
+    return {
+      title: "Портфолио",
+    }
+  },
+  data() {
+    return {
+      properties: {
+        developer: 0,
+        designer: 0,
+        entrepreneur: 0,
+      },
+    }
+  },
+  async asyncData({ $strapi, store }) {
+    const portfolio = store.state.gql.portfolio
+    const data = (
+      await $strapi.graphql({
+        query: portfolio,
+      })
+    )?.page?.data?.attributes
 
-		return {
-			data,
-		}
-	},
-	mounted() {
-		this.setAnimation()
-		//setTimeout(() => this.setAnimation(), 500)
-	},
-	methods: {
-		setAnimation() {
-			//this.$anime({
-			//	targets: this.properties,
-			//	developer: 100,
-			//	designer: 200,
-			//	entrepreneur: 300,
-			//	easing: 'linear',
-			//	duration: () => this.$anime.random(2300, 4200),
-			//	delay: () => this.$anime.random(550, 850),
-			//	round: 1,
-			//	//update: function () {
-			//	//	//JSON.stringify(this.properties)
-			//	//},
-			//})
-			//console.log(this.$anime.random(0, 270))
-			//this.$anime({
-			//	targets: '.morphing-demo .polymorph',
-			//	points: [
-			//		{
-			//			value: [
-			//				'70 24 119.574 60.369 100.145 117.631 50.855 101.631 3.426 54.369',
-			//				'70 41 118.574 59.369 111.145 132.631 60.855 84.631 20.426 60.369',
-			//			],
-			//		},
-			//		{ value: '70 6 119.574 60.369 100.145 117.631 39.855 117.631 55.426 68.369' },
-			//		{ value: '70 57 136.574 54.369 89.145 100.631 28.855 132.631 38.426 64.369' },
-			//		{ value: '70 24 119.574 60.369 100.145 117.631 50.855 101.631 3.426 54.369' },
-			//	],
-			//	easing: 'easeOutQuad',
-			//	duration: 2000,
-			//	loop: true,
-			//})
-		},
-		redirect(url) {
-			location.href = '/' + url
-		},
-	},
+    return {
+      data,
+    }
+  },
+  mounted() {
+    this.setAnimation()
+    //setTimeout(() => this.setAnimation(), 500)
+  },
+  methods: {
+    setAnimation() {
+      //this.$anime({
+      //	targets: this.properties,
+      //	developer: 100,
+      //	designer: 200,
+      //	entrepreneur: 300,
+      //	easing: 'linear',
+      //	duration: () => this.$anime.random(2300, 4200),
+      //	delay: () => this.$anime.random(550, 850),
+      //	round: 1,
+      //	//update: function () {
+      //	//	//JSON.stringify(this.properties)
+      //	//},
+      //})
+      //console.log(this.$anime.random(0, 270))
+      //this.$anime({
+      //	targets: '.morphing-demo .polymorph',
+      //	points: [
+      //		{
+      //			value: [
+      //				'70 24 119.574 60.369 100.145 117.631 50.855 101.631 3.426 54.369',
+      //				'70 41 118.574 59.369 111.145 132.631 60.855 84.631 20.426 60.369',
+      //			],
+      //		},
+      //		{ value: '70 6 119.574 60.369 100.145 117.631 39.855 117.631 55.426 68.369' },
+      //		{ value: '70 57 136.574 54.369 89.145 100.631 28.855 132.631 38.426 64.369' },
+      //		{ value: '70 24 119.574 60.369 100.145 117.631 50.855 101.631 3.426 54.369' },
+      //	],
+      //	easing: 'easeOutQuad',
+      //	duration: 2000,
+      //	loop: true,
+      //})
+    },
+    redirect(url) {
+      location.href = "/" + url
+    },
+  },
 }
 </script>
 
