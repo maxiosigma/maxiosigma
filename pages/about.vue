f68f3f9f5af8398741e4dbafac2d9b28722f11dc
MaxEros <gtxuss@gmail.com>
2022-06-26 20:26:47 +0300

    backup

--- pages/about.vue
+++ pages/about.vue
@@ -24,24 +24,31 @@
       </div>
 
       <div class="flex-col text-white inline-flex place-items-start self-center about-text">
-        <div class="">Добро пожаловать</div>
-        <div class="">Меня зовут Максим</div>
-        <div class="">Автор и разработчик сайта</div>
+        <div class="font-black mb-2 text-2xl tracking-[5px] uppercase">Welcome</div>
+        <div class="font-black text-md tracking-[2px] uppercase">← Максим</div>
+        <div class="font-black text-md mb-3 tracking-[2px] uppercase">
+          ← <span class="text-xs">Автор и разработчик сайта</span>
+        </div>
+
+        <div class="font-black text-md tracking-[1px] uppercase">
+          ▬ <span class="text-xs">Желаете узнать подробнее ?</span>
+        </div>
 
         <div
           class="
             rounded-md
             cursor-pointer
             bg-green-700
-            mt-1
-            text-white
-            px-4 px-2
+            mt-4
+            text-white text-xl
+            py-2
+            px-4
             duration-300
             pointer-events-auto
             hover:(bg-yellow-600)
           "
         >
-          <div>Кнопка</div>
+          <div>Да</div>
         </div>
       </div>
     </div>
