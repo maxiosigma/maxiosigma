d05001df71005bb350d7d6be4028dcd1f47a8ed6
MaxEros <gtxuss@gmail.com>
2022-06-26 20:40:35 +0300

    backup

--- pages/about.vue
+++ pages/about.vue
@@ -1,11 +1,11 @@
 <template>
   <LayoutPage :bodyStyle="'body-bg about'">
-    <div :class="['about-cont', { '': visible === 0.5 }, { hidden: visible === 1 }]">
+    <div :class="['about-cont', { '': visible === 0.5 }, { '!hidden': visible === 1 }]">
       <div class="about-img-cont">
         <ItemImg class="about-img" src="about/face_1.png"></ItemImg>
       </div>
 
-      <div :class="['about-field', { '': visible === 0.5 }, { hidden: visible === 1 }]">
+      <div :class="['about-field', { '': visible === 0.5 }, { '!hidden': visible === 1 }]">
         <div class="font-black mb-5 text-2xl tracking-[5px] uppercase">Welcome</div>
         <div class="font-black text-md tracking-[2px] uppercase">← Максим</div>
         <div class="font-black text-md tracking-[2px] uppercase">
@@ -40,6 +40,8 @@ export default {
     handleClick() {
       this.visible += 0.5
       setTimeout(() => (this.visible += 1), 1500)
+
+      console.log(this.visible)
     },
   },
 }
