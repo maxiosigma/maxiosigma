1400f13141de0fba70a6a4898ee481822ef5ad1d
MaxEros <gtxuss@gmail.com>
2022-06-26 20:36:03 +0300

    backup

--- pages/about.vue
+++ pages/about.vue
@@ -1,55 +1,22 @@
 <template>
   <LayoutPage :bodyStyle="'body-bg about'">
-    <div
-      class="
-        container
-        flex-grow
-        h-full
-        mx-auto
-        max-h-full
-        z-10
-        gap-5
-        grid-cols-[2fr,1fr]
-        relative
-        inline-grid
-        overflow-hidden
-        pointer-events-none
-      "
-    >
-      <div class="flex justify-center about-img-container items-end">
-        <ItemImg
-          class="h-full object-cover object-center about-img"
-          src="about/face_1.png"
-        ></ItemImg>
+    <div class="about-cont">
+      <div class="about-img-cont">
+        <ItemImg class="about-img" src="about/face_1.png"></ItemImg>
       </div>
 
       <div class="flex-col text-white inline-flex place-items-start self-center about-text">
-        <div class="font-black mb-2 text-2xl tracking-[5px] uppercase">Welcome</div>
+        <div class="font-black mb-5 text-2xl tracking-[5px] uppercase">Welcome</div>
         <div class="font-black text-md tracking-[2px] uppercase">← Максим</div>
-        <div class="font-black text-md mb-3 tracking-[2px] uppercase">
+        <div class="font-black text-md tracking-[2px] uppercase">
           ← <span class="text-xs">Автор и разработчик сайта</span>
         </div>
 
-        <div class="font-black text-md tracking-[1px] uppercase">
+        <div class="font-black mt-4 text-md mb-5 tracking-[1.5px] uppercase">
           ▬ <span class="text-xs">Желаете узнать подробнее ?</span>
         </div>
 
-        <div
-          class="
-            rounded-md
-            cursor-pointer
-            bg-green-700
-            mt-4
-            text-white text-xl
-            py-2
-            px-4
-            duration-300
-            pointer-events-auto
-            hover:(bg-yellow-600)
-          "
-        >
-          <div>Да</div>
-        </div>
+        <div class="about-btn">Да</div>
       </div>
     </div>
 
@@ -80,6 +47,20 @@ export default {
 
 <style lang="scss">
 .about {
-  //@apply flex flex-col -mt-20 z-10 relative <lg: (flex-wrap pt-5 pb-10) ;
+  &-cont {
+    @apply container flex-grow h-full mx-auto max-h-full z-10 gap-5 grid-cols-[1.7fr,1fr] relative inline-grid overflow-hidden pointer-events-none;
+  }
+
+  &-img {
+    @apply h-full object-cover object-center;
+
+    &-cont {
+      @apply flex justify-center items-end;
+    }
+  }
+
+  &-btn {
+    @apply rounded-md cursor-pointer bg-green-700 text-white text-xl py-2 px-4 transition-all duration-300 pointer-events-auto hover:(bg-yellow-600 px-6 py-3 text-shadow) ;
+  }
 }
 </style>
