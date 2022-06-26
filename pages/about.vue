76786007be261e587983a1fd198daa4a4340468c
MaxEros <gtxuss@gmail.com>
2022-06-26 13:20:45 +0300

    backup

--- pages/about.vue
+++ pages/about.vue
@@ -18,23 +18,19 @@
     <kinesis-container class="flex-center text-white inset-0 z-0 fixed">
       <!-- class="bg-black" -->
 
-      <!--     -->
-      <div
-        class="absolute"
-        v-for="(it, i) in 2"
-        :key="i"
-        v-anime="{
-          translateX: Math.sin(i + 1) * 100,
-          translateY: Math.cos(i + 1) * 100,
+      <!-- v-anime="{
+          translateX: Math.sin(i + 1) * intRandom(50, 150),
+          translateY: Math.cos(i + 1) * intRandom(50, 150),
           duration: 1500,
-        }"
-      >
-        <kinesis-element
+          delay: 1000,
+        }"   -->
+      <div class="absolute" v-for="(it, i) in 2" :key="i">
+        <!--<kinesis-element
           :strength="intRandom(15, 55)"
           :type="stringRandom(['depth', 'depth_inv', 'translate', 'rotate'])"
-        >
-          <canvas class="flex-center" :ref="'canva_' + i"></canvas>
-        </kinesis-element>
+        >-->
+        <canvas class="flex-center" :ref="'canva_' + i"></canvas>
+        <!--</kinesis-element>-->
       </div>
 
       <!--<kinesis-element type="depth" :strength="50">
@@ -65,13 +61,15 @@ export default {
   mounted() {
     console.log(this.$refs?.canva_0[0])
 
+    const center = { x: (300 - 25) / 2, y: (150 - 25) / 2 }
+
     const trangle = this.$refs?.canva_0?.[0]
     this.isDraw(trangle, () => {
       const ctx = trangle.getContext("2d")
       ctx.beginPath()
-      ctx.moveTo(25, 25)
-      ctx.lineTo(25, 9)
-      ctx.lineTo(9, 25)
+      ctx.moveTo(center.x + 25, center.y + 25)
+      ctx.lineTo(center.x + 25, center.y + 9)
+      ctx.lineTo(center.x + 9, center.y + 25)
       ctx.closePath()
       ctx.strokeStyle = "orange"
       ctx.lineWidth = 3
