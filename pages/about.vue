81d209d6e940590165ace05dd3e4253072ef66c7
MaxEros <gtxuss@gmail.com>
2022-06-26 14:07:36 +0300

    backup

--- pages/about.vue
+++ pages/about.vue
@@ -26,7 +26,7 @@
         }"   -->
       <div
         class="absolute"
-        v-for="(it, i) in 2"
+        v-for="(it, i) in 5"
         :key="i"
         v-anime="{
           translateX: Math.sin(i + 1) * intRandom(50, 150),
@@ -113,7 +113,7 @@ export default {
     },
     preDraw(obj) {
       const center = { x: (300 - 25) / 2, y: (150 - 25) / 2 }
-      const color = this.stringRandom(["#FFA500", "#00ff2a", "#0084ff", "#9d00ff", "#ff0055"])
+      //const color = this.stringRandom(["#FFA500", "#00ff2a", "#0084ff", "#9d00ff", "#ff0055"])
       const ctx = obj?.getContext("2d")
 
       const presets = [
@@ -124,7 +124,13 @@ export default {
             ctx.lineTo(center.x + 25, center.y + 9)
             ctx.lineTo(center.x + 9, center.y + 25)
             ctx.closePath()
-            ctx.strokeStyle = color
+            ctx.strokeStyle = this.stringRandom([
+              "#FFA500",
+              "#00ff2a",
+              "#0084ff",
+              "#9d00ff",
+              "#ff0055",
+            ])
             ctx.lineWidth = 3
             ctx.stroke()
           })
@@ -137,7 +143,13 @@ export default {
             ctx.lineTo(center.x + 16, center.y + 16)
             ctx.lineTo(center.x, center.y + 16)
             ctx.closePath()
-            ctx.strokeStyle = color
+            ctx.strokeStyle = this.stringRandom([
+              "#FFA500",
+              "#00ff2a",
+              "#0084ff",
+              "#9d00ff",
+              "#ff0055",
+            ])
             ctx.lineWidth = 3
             ctx.stroke()
           })
@@ -146,7 +158,13 @@ export default {
           this.isDraw(obj, () => {
             ctx.beginPath()
             ctx.arc(25, 25, 9, 0, Math.PI, true)
-            ctx.strokeStyle = color
+            ctx.strokeStyle = this.stringRandom([
+              "#FFA500",
+              "#00ff2a",
+              "#0084ff",
+              "#9d00ff",
+              "#ff0055",
+            ])
             ctx.lineWidth = 3
             ctx.stroke()
           })
@@ -155,7 +173,7 @@ export default {
 
       const rand = this.intRandom(0, presets.length - 1)
 
-      return presets[rand]
+      return presets[rand](obj)
     },
   },
 }
