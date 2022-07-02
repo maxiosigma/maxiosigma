499f5de874e33ac018e386b93649630f1bbca923
MaxEros <gtxuss@gmail.com>
2022-07-03 00:21:12 +0300

    backup

--- components/Template/Preload.vue
+++ components/Template/Preload.vue
@@ -39,15 +39,15 @@ export default {
     animation: preload-sphere 5.88235294117647s cubic-bezier(0, 0.2, 0.8, 1) infinite;
 
     &:nth-child(1) {
-      @apply border-yellow-300 animate-delay-0;
+      @apply border-cyan-400 border-opacity-30 animate-delay-0;
     }
 
     &:nth-child(2) {
-      @apply border-pink-400 animate-delay-[-2.941176470588235s];
+      @apply border-fuchsia-400 border-opacity-30 animate-delay-[-2.941176470588235s];
     }
 
     &:nth-child(3) {
-      @apply border-green-400 animate-delay-[-4.041176470588235s];
+      @apply border-green-400 border-opacity-30 animate-delay-[-4.041176470588235s];
     }
   }
 
