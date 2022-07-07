f47672338509e239839099df711b198a55bc6f24
MaxEros <gtxuss@gmail.com>
2022-07-08 02:48:18 +0300

    backup

--- pages/programs/_page.vue
+++ pages/programs/_page.vue
@@ -32,7 +32,7 @@
               v-anime="{
                 loop: true,
                 opacity: 0,
-                duration: j * 5000,
+                duration: (j + 1) * 5000,
                 delay: j * 5000,
               }"
             />
