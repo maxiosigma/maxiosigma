78b9eab46fc07e0c81adebda481548609ae8bfc0
maxiosigma <gtxyd@mail.ru>
2022-10-03 13:19:10 +0300

    bk test

--- nuxt.config.js
+++ nuxt.config.js
@@ -356,7 +356,6 @@ export default {
                   })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "margika");
 
                   margika(89264491, "init", { clickmap: true, trackLinks: true, accurateTrackBounce: true, webvisor: true });
-                  // aaaaaaaaaaa
 
                   console.log("YM PIXEL ACTIVE");
                }, 250);
