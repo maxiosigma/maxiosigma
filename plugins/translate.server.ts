0abdc674a601e8619d7e4ebe2a0f25f2824a68bb
maxiosigma <gtxyd@mail.ru>
2023-04-07 14:08:09 +0300

    bk

--- plugins/translate.server.ts
+++ plugins/translate.server.ts
@@ -24,139 +24,188 @@ export default defineNuxtPlugin(async (nuxtApp) => {
     const { asyncReduceArray, asyncReduceObject } = useFunctions()
 
     const navSlugs = ['nav', 'footer', 'social']
+    const defaultLang = 'ru'
+    const langs = ['ru', 'en', 'zh']
 
-    const ru = {
-        links:
-            (await graphql(data.links()))?.data.links.data.map(({ attributes }) => ({
-                ferd: useCripty(attributes.href),
-                sh: attributes.short,
-            })) || [],
-        reffers:
-            (await graphql(data.links()))?.data.links.data
-                .filter(({ attributes: link }) => !!link?.partnership && !!link?.title && !!link?.description && !!link?.short)
-                .map(({ attributes: link }) => ({
-                    title: link?.title,
-                    description: link?.description,
-                    images: link?.imgs?.data?.map(({ attributes }) => attributes),
-                    short: link?.short,
-                    tags: link?.tags?.data?.map(({ attributes: { title } }) => title)?.sort((a, b) => (a?.length > b?.length ? 1 : -1)),
-                    top: link?.top,
-                })) || [],
-        works:
-            (await graphql(data.works()))?.data.works.data
-                .map(({ attributes }) => ({
-                    ...attributes,
-                    assets: {
-                        fonts: attributes.assets.fonts.data.map((as) => as.attributes.title),
-                        models: attributes.assets.models.data.map((as) => as.attributes.title),
-                        technologies: attributes.assets.technologies.data.map((as) => as.attributes.title),
-                    },
-                    media: attributes.media.data.map((md) => {
-                        const alt = md.attributes.alternativeText
-                        delete md.attributes.alternativeText
-                        return { ...md.attributes, alt }
-                    }),
-                }))
-                .reverse() || [],
-        publics: (await graphql(data.publics()))?.data.publicateds.data.map((it) => it.attributes) || [],
-        menu: await asyncReduceObject(navSlugs, async (it) => ({
-            [it]: (await graphql(data.menu(it)))?.data.renderNavigation,
-        })),
-    }
+    const fieldTranslate = async (field = '', lang = '') => (lang === defaultLang || !field ? field : await translate(field, { from: 'ru', to: lang }))
 
-    const en = {
-        links: read('en/links') ?? ru.links,
-        reffers:
-            read('en/reffers') ??
-            (await asyncReduceArray(ru.reffers, async (it) => [
-                {
-                    ...it,
-                    title: it.title ? await translate(it.title, { from: 'ru', to: 'en' }) : null,
-                    description: it.description ? await translate(it.description, { from: 'ru', to: 'en' }) : null,
-                },
-            ])),
-        works:
-            read('en/works') ??
-            (await asyncReduceArray(ru.works, async (it, i) => [
-                {
-                    ...it,
-                    title: it.title ? await translate(it.title, { from: 'ru', to: 'en' }) : null,
-                    description: it.description ? await translate(it.description, { from: 'ru', to: 'en' }) : null,
-                },
-            ])),
-        publics:
-            read('en/publics') ??
-            (await asyncReduceArray(ru.publics, async (w, i) => {
-                return [
+    const content = await asyncReduceArray(langs, async (lang = '') => {
+        const links = read(`${lang}/links`) ?? (await graphql(data.links()))?.data.links.data.map(({ attributes: link = {} }) => ({ ferd: useCripty(link?.href), sh: link?.short }))
+        const reffers =
+            read(`${lang}/reffers`) ??
+            (await asyncReduceArray(
+                (await graphql(data.links()))?.data.links.data.filter(({ attributes: link = {} }) => !!link?.partnership && !!link?.title && !!link?.description && !!link?.short),
+                async ({ attributes: link = {} }) => [
                     {
-                        ...w,
-                        title: w.title ? await translate(w.title, { from: 'ru', to: 'en' }) : null,
-                        description: w.description ? await translate(w.description, { from: 'ru', to: 'en' }) : null,
-                        keywords: w.keywords ? await translate(w.keywords, { from: 'ru', to: 'en' }) : null,
+                        title: await fieldTranslate(link?.title, lang),
+                        description: await fieldTranslate(link?.description, lang),
+                        images: link?.imgs?.data?.map(({ attributes }) => attributes),
+                        short: link?.short,
+                        tags: link?.tags?.data?.map(({ attributes: { title } }) => title)?.sort((a, b) => (a?.length > b?.length ? 1 : -1)),
+                        top: link?.top,
                     },
                 ]
-            })),
-        menu:
-            read('en/menu') ??
-            (await asyncReduceObject(Object.entries(ru.menu), async ([key, values]) => ({
-                [key]: await asyncReduceArray(values, async (v) => [{ ...v, title: v?.title ? await translate(v?.title, { from: 'ru', to: 'en' }) : null }]),
-            }))),
-    }
+            ))
 
-    const zh = {
-        links: read('zh/links') ?? ru.links,
-        reffers:
-            read('en/reffers') ??
-            (await asyncReduceArray(ru.reffers, async (it) => [
-                {
-                    ...it,
-                    title: it.title ? await translate(it.title, { from: 'ru', to: 'zh' }) : null,
-                    description: it.description ? await translate(it.description, { from: 'ru', to: 'zh' }) : null,
-                },
-            ])),
-        works:
-            read('zh/works') ??
-            (await asyncReduceArray(ru.works, async (w, i) => {
-                return [
+        const works =
+            read(`${lang}/works`) ??
+            (
+                await asyncReduceArray((await graphql(data.works()))?.data.works.data, async ({ attributes: work }) => [
                     {
-                        ...w,
-                        title: w.title ? await translate(w.title, { from: 'ru', to: 'zh' }) : null,
-                        description: w.description ? await translate(w.description, { from: 'ru', to: 'zh' }) : null,
+                        ...work,
+                        title: await fieldTranslate(work?.title, lang),
+                        description: await fieldTranslate(work?.description, lang),
+                        assets: {
+                            fonts: work?.assets.fonts.data.map((it) => it?.attributes.title),
+                            models: work?.assets.models.data.map((it) => it?.attributes.title),
+                            technologies: work?.assets.technologies.data.map((it) => it?.attributes.title),
+                        },
+                        media: work?.media.data.map((it) => {
+                            const alt = it?.attributes.alternativeText
+                            delete it.attributes.alternativeText
+                            return { ...it?.attributes, alt }
+                        }),
                     },
-                ]
-            })),
-        publics:
-            read('zh/publics') ??
-            (await asyncReduceArray(ru.publics, async (w, i) => {
-                return [
-                    {
-                        ...w,
-                        title: w.title ? await translate(w.title, { from: 'ru', to: 'zh' }) : null,
-                        description: w.description ? await translate(w.description, { from: 'ru', to: 'zh' }) : null,
-                        keywords: w.keywords ? await translate(w.keywords, { from: 'ru', to: 'zh' }) : null,
-                    },
-                ]
-            })),
-        menu:
-            read('zh/menu') ??
-            (await asyncReduceObject(Object.entries(ru.menu), async ([key, values]) => ({
-                [key]: await asyncReduceArray(values, async (v) => [{ ...v, title: v?.title ? await translate(v?.title, { from: 'ru', to: 'zh' }) : null }]),
-            }))),
-    }
+                ])
+            )?.reverse()
 
-    const result = { ru, en, zh }
-
-    Object.values(result).map((it, i) => {
-        const l = Object.keys(result)[i]
-        const k = Object.keys(it)
-
-        k.map((kit) => {
-            write(`${l}/${kit}`, it?.[kit])
-        })
+        write(`${lang}/links`, links)
+        write(`${lang}/reffers`, reffers)
+        write(`${lang}/works`, works)
     })
-
-    nuxtApp.payload.data = result
 })
+
+//const en = {
+//    links: read('en/links') ?? ru.links,
+//    reffers:
+//        read('en/reffers') ??
+//        (await asyncReduceArray(ru.reffers, async (it) => [
+//            {
+//                ...it,
+//                title: it.title ? await translate(it.title, { from: 'ru', to: 'en' }) : null,
+//                description: it.description ? await translate(it.description, { from: 'ru', to: 'en' }) : null,
+//            },
+//        ])),
+//    works:
+//        read('en/works') ??
+//        (await asyncReduceArray(ru.works, async (it, i) => [
+//            {
+//                ...it,
+//                title: it.title ? await translate(it.title, { from: 'ru', to: 'en' }) : null,
+//                description: it.description ? await translate(it.description, { from: 'ru', to: 'en' }) : null,
+//            },
+//        ])),
+//    publics:
+//        read('en/publics') ??
+//        (await asyncReduceArray(ru.publics, async (w, i) => {
+//            return [
+//                {
+//                    ...w,
+//                    title: w.title ? await translate(w.title, { from: 'ru', to: 'en' }) : null,
+//                    description: w.description ? await translate(w.description, { from: 'ru', to: 'en' }) : null,
+//                    keywords: w.keywords ? await translate(w.keywords, { from: 'ru', to: 'en' }) : null,
+//                },
+//            ]
+//        })),
+//    menu:
+//        read('en/menu') ??
+//        (await asyncReduceObject(Object.entries(ru.menu), async ([key, values]) => ({
+//            [key]: await asyncReduceArray(values, async (v) => [{ ...v, title: v?.title ? await translate(v?.title, { from: 'ru', to: 'en' }) : null }]),
+//        }))),
+//}
+
+//const zh = {
+//    links: read('zh/links') ?? ru.links,
+//    reffers:
+//        read('en/reffers') ??
+//        (await asyncReduceArray(ru.reffers, async (it) => [
+//            {
+//                ...it,
+//                title: it.title ? await translate(it.title, { from: 'ru', to: 'zh' }) : null,
+//                description: it.description ? await translate(it.description, { from: 'ru', to: 'zh' }) : null,
+//            },
+//        ])),
+//    works:
+//        read('zh/works') ??
+//        (await asyncReduceArray(ru.works, async (w, i) => {
+//            return [
+//                {
+//                    ...w,
+//                    title: w.title ? await translate(w.title, { from: 'ru', to: 'zh' }) : null,
+//                    description: w.description ? await translate(w.description, { from: 'ru', to: 'zh' }) : null,
+//                },
+//            ]
+//        })),
+//    publics:
+//        read('zh/publics') ??
+//        (await asyncReduceArray(ru.publics, async (w, i) => {
+//            return [
+//                {
+//                    ...w,
+//                    title: w.title ? await translate(w.title, { from: 'ru', to: 'zh' }) : null,
+//                    description: w.description ? await translate(w.description, { from: 'ru', to: 'zh' }) : null,
+//                    keywords: w.keywords ? await translate(w.keywords, { from: 'ru', to: 'zh' }) : null,
+//                },
+//            ]
+//        })),
+//    menu:
+//        read('zh/menu') ??
+//        (await asyncReduceObject(Object.entries(ru.menu), async ([key, values]) => ({
+//            [key]: await asyncReduceArray(values, async (v) => [{ ...v, title: v?.title ? await translate(v?.title, { from: 'ru', to: 'zh' }) : null }]),
+//        }))),
+//}
+
+//const result = { ru, en, zh }
+
+//Object.values(result).map((it, i) => {
+//    const l = Object.keys(result)[i]
+//    const k = Object.keys(it)
+
+//    k.map((kit) => {
+//        write(`${l}/${kit}`, it?.[kit])
+//    })
+//})
+
+//const ru = {
+//    links:
+//        (await graphql(data.links()))?.data.links.data.map(({ attributes }) => ({
+//            ferd: useCripty(attributes.href),
+//            sh: attributes.short,
+//        })) || [],
+//    reffers:
+//        (await graphql(data.links()))?.data.links.data
+//            .filter(({ attributes: link }) => !!link?.partnership && !!link?.title && !!link?.description && !!link?.short)
+//            .map(({ attributes: link }) => ({
+//                title: link?.title,
+//                description: link?.description,
+//                images: link?.imgs?.data?.map(({ attributes }) => attributes),
+//                short: link?.short,
+//                tags: link?.tags?.data?.map(({ attributes: { title } }) => title)?.sort((a, b) => (a?.length > b?.length ? 1 : -1)),
+//                top: link?.top,
+//            })) || [],
+//    works:
+//        (await graphql(data.works()))?.data.works.data
+//            .map(({ attributes }) => ({
+//                ...attributes,
+//                assets: {
+//                    fonts: attributes.assets.fonts.data.map((as) => as.attributes.title),
+//                    models: attributes.assets.models.data.map((as) => as.attributes.title),
+//                    technologies: attributes.assets.technologies.data.map((as) => as.attributes.title),
+//                },
+//                media: attributes.media.data.map((md) => {
+//                    const alt = md.attributes.alternativeText
+//                    delete md.attributes.alternativeText
+//                    return { ...md.attributes, alt }
+//                }),
+//            }))
+//            .reverse() || [],
+//    publics: (await graphql(data.publics()))?.data.publicateds.data.map((it) => it.attributes) || [],
+//    menu: await asyncReduceObject(navSlugs, async (it) => ({
+//        [it]: (await graphql(data.menu(it)))?.data.renderNavigation,
+//    })),
+//}
+
+//nuxtApp.payload.data = result
 
 //nuxtApp.payload.data = {
 //    ...useNuxtApp().payload.data,
