e5a68b64ef3851ee00473096e9363c4ed4a6f57b
maxiosigma <gtxyd@mail.ru>
2023-05-02 13:17:35 +0300

    bk

--- windi.config.ts
+++ windi.config.ts
@@ -1,1 +1,36 @@
-import { extract, plugins, shortcuts, theme, utilities, variants } from 'maxiosigma_libs/windi'
+import { functions } from 'maxiosigma_libs/index'
+import { extract, plugins, shortcuts, theme } from 'maxiosigma_libs/windi'
+
+const { range } = functions
+
+const {
+    base: { defineConfig },
+    built: { aspectRatio, lineClamp, typography },
+    external: { animations, heropatterns, icons, questionMark },
+    transform: { tailwindElevation },
+} = plugins
+
+export default defineConfig({
+    shortcuts,
+    attributify: false,
+    theme: {
+        extend: theme.extend,
+    },
+    safelist: [
+        //
+        range(10).map((i) => `order-${i}`),
+        range(20).map((i) => `pl-[${i}%]`),
+    ],
+    plugins: [
+        //
+        aspectRatio,
+        lineClamp,
+        typography,
+        animations,
+        heropatterns,
+        icons,
+        questionMark,
+        tailwindElevation,
+    ],
+    extract: extract.nuxt,
+})
