6d9c43b0093c1598547a758330bde22347c4beed
Maxim Semenuk <gtxyss@gmail.com>
2023-11-10 03:40:41 +0300

    bk

--- pages/index.vue
+++ pages/index.vue
@@ -7,23 +7,24 @@
                 <ItemMediaImg
                     :class="[
                         'absolute h-full w-full ml-10 object-cover object-right',
-                        timers.mainImg
-                            ? 'animated animate-fade-in'
-                            : 'animated animate-fade-out',
+                        //timers.mainImg
+                        //    ? 'animated animate-fade-in'
+                        //    : 'animated animate-fade-out',
                     ]"
                     src="main/face_1.webp"
                 ></ItemMediaImg>
             </div>
 
-            {{ timers }}
-
             <div :class="['relative flex-grow h-full w-full overflow-hidden']">
                 <ItemMediaImg
                     :class="[
                         'absolute h-full w-full -ml-10 object-cover object-left',
-                        !timers.mainImg
-                            ? 'animated animate-fade-in'
-                            : 'animated animate-fade-out',
+                        //!timers.mainImg
+                        //    ? 'animated animate-fade-in'
+                        //    : 'animated animate-fade-out',
+                        //timers.mainImgCount === 0
+                        //    ? 'opacity-0 animate-none'
+                        //    : '',
                     ]"
                     src="main/face_2.webp"
                 ></ItemMediaImg>
@@ -35,12 +36,11 @@
 </template>
 
 <script setup>
-const timers = ref({ mainImg: true })
-
-setInterval(async function () {
-    timers.value.mainImg = !timers.value.mainImg
-    console.log(timers.value)
-}, 5000)
+//const timers = ref({ mainImg: false, mainImgCount: 0 })
+//setInterval(async function () {
+//    timers.value.mainImg = !timers.value.mainImg
+//    timers.value.mainImgCount === 0 ? timers.value.mainImgCount++ : null
+//}, 5000)
 </script>
 
 <style lang="scss">
