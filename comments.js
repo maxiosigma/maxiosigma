16ffb57ef60c314591ebead0606b9d25aee1aea0
Maxim Semenuk <gtxyss@gmail.com>
2023-11-10 03:29:26 +0300

    bk

--- comments.js
+++ comments.js
@@ -152,3 +152,36 @@
 //  ></div>
 //</div>
 //</div>-->
+
+/* INDEX
+
+
+//const { query } = useRoute()
+//const { x, y, sourceType } = useMouse({ touch: false })
+//const { itemIsArray } = useFunctions()
+//const { isMobile } = useDevice()
+
+//const { t } = useI18n()
+//const { localeProperties: lp } = useI18n()
+//const lang = ref(lp.value.code)
+
+//const title = ref('~ MAIN ~')
+//const description = ref('')
+
+//const activeSlide = ref('FAQ')
+//const isQuery = useQueryLength() !== 0 && !useUtm(query)
+
+//const isActive = (i) => activeSlide.value === i
+
+//const { data } = useNuxtData()
+
+//data.value = {
+//    work_types: await useContentData(lang.value, 'work_types'),
+//    work_categories: await useContentData(lang.value, 'work_categories'),
+//}
+
+//if (isQuery) navigateTo({ path: '/link', query })
+//useHead({ title, description })
+
+
+INDEX */
--- pages/index.vue
+++ pages/index.vue
@@ -3,16 +3,26 @@
         <!--  -->
 
         <div class="flex flex-grow">
-            <div class="relative flex-grow h-full w-full">
+            <div :class="['relative flex-grow h-full w-full overflow-hidden']">
                 <ItemMediaImg
-                    class="absolute h-full w-full ml-13 object-cover object-right"
+                    :class="[
+                        'absolute h-full w-full ml-10 object-cover object-right',
+                        timers.mainImgLeft
+                            ? 'animated animate-fade-in'
+                            : 'animated animate-fade-out',
+                    ]"
                     src="main/face_1.webp"
                 ></ItemMediaImg>
             </div>
 
-            <div class="relative flex-grow h-full w-full">
+            <div :class="['relative flex-grow h-full w-full overflow-hidden']">
                 <ItemMediaImg
-                    class="absolute h-full w-full -ml-13 object-cover object-left"
+                    :class="[
+                        'absolute h-full w-full -ml-10 object-cover object-left',
+                        timers.mainImgRight
+                            ? 'animated animate-fade-in'
+                            : 'animated animate-fade-out',
+                    ]"
                     src="main/face_2.webp"
                 ></ItemMediaImg>
             </div>
@@ -23,32 +33,12 @@
 </template>
 
 <script setup>
-//const { query } = useRoute()
-//const { x, y, sourceType } = useMouse({ touch: false })
-//const { itemIsArray } = useFunctions()
-//const { isMobile } = useDevice()
+const timers = { mainImgLeft: true, mainImgRight: false }
 
-//const { t } = useI18n()
-//const { localeProperties: lp } = useI18n()
-//const lang = ref(lp.value.code)
-
-//const title = ref('~ MAIN ~')
-//const description = ref('')
-
-//const activeSlide = ref('FAQ')
-//const isQuery = useQueryLength() !== 0 && !useUtm(query)
-
-//const isActive = (i) => activeSlide.value === i
-
-//const { data } = useNuxtData()
-
-//data.value = {
-//    work_types: await useContentData(lang.value, 'work_types'),
-//    work_categories: await useContentData(lang.value, 'work_categories'),
-//}
-
-//if (isQuery) navigateTo({ path: '/link', query })
-//useHead({ title, description })
+setInterval(() => {
+    timers.mainImgLeft = !timers.mainImgLeft
+    timers.mainImgRight = !timers.mainImgRight
+}, 3000)
 </script>
 
 <style lang="scss">
