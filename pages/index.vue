ca8654c0f133fc42f61f067c62450ea22840aea6
Maxim Semenuk <gtxyss@gmail.com>
2023-11-12 03:16:43 +0300

    bk

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,13 @@
     <LayoutPage pagecl="main">
         <!--  -->
 
-        <div class="mt-auto mx-auto mb-4">{{ clip }}</div>
+        <div class="m-auto">{{ polygon }}</div>
+        <!--<div class="mt-auto mx-auto mb-4">{{ basePolygon }}</div>-->
 
-        <div
+        <!--<div
             class="main-clip self-center mb-auto mx-auto bg-dark-100 h-30vh w-30vw overflow-hidden"
             :style="`clip-path: polygon(${clip})`"
-        ></div>
+        ></div>-->
 
         <!--<div class="flex flex-grow">
             <div :class="['relative flex-grow h-full w-full overflow-hidden']">
@@ -44,20 +45,47 @@
 </template>
 
 <script setup>
-const clip = [
+const polygon = [
     [0, 0],
-    [100, 0],
-    [100, 100],
-    [0, 100],
+    [100, 0]
 ]
-    .reduce((s, [x, y], i) => {
-        s.push([x, y])
-        s.push([x * 0.5, y * 0.2])
 
-        return s
-    }, [])
-    .map(([x, y]) => `${x}% ${y}%`)
-    .join(', ')
+const dotHF = ([x1, y1], [x2, y2]) => [x1 + (x2 - x1) / 2, y1 + (y2 - y1) / 2]
+
+const newDot = (i1, i2) => polygon.splice(i2, 0, dotHF(polygon[i1], polygon[i2]))
+
+const newDotsBetween = () => {
+    newDot(polygon.length - 2, polygon.length - 1)
+    newDot(0, 1)
+}
+
+//newDot(0, 1)
+//newDot(0, 1)
+//newDot(2, 3)
+//newDot(3, 4)
+//newDot(0, 1)
+//newDot(0, 1)
+
+newDotsBetween()
+
+//newDotsBetween()
+
+console.log(polygon)
+
+//const clip = [
+//    [0, 0],
+//    [100, 0],
+//    [100, 100],
+//    [0, 100],
+//]
+//    .reduce((s, [x, y], i) => {
+//        s.push([x, y])
+//        s.push([x * 0.5, y * 0.2])
+
+//        return s
+//    }, [])
+//    .map(([x, y]) => `${x}% ${y}%`)
+//    .join(', ')
 
 //const timers = ref({ mainImg: false, mainImgCount: 0 })
 //setInterval(async function () {
