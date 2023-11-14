29098032fa3ed5674a113028f48552b73aa0ff16
Maxim Semenuk <gtxyss@gmail.com>
2023-11-14 19:39:01 +0300

    bk

--- app.comments.ts
+++ app.comments.ts
@@ -663,3 +663,104 @@
 //    //router.push('/dashboard')
 //    //signInWithPassword
 //}
+
+// ---------------------------------------------
+
+import { pointsOnBezierCurves } from 'points-on-curve'
+
+console.log(
+    pointsOnBezierCurves([
+        [5, 5],
+        [10, 10],
+        [15, 5],
+        [20, 0]
+    ])
+)
+
+const polygon_test = [
+    [0, 0],
+    ...pointsOnBezierCurves([
+        [1, 0],
+        [1.5, 4],
+        [2.5, 4],
+        [3, 0],
+        [3.5, 4],
+        [4.5, 4],
+        [5, 0]
+    ]),
+    [100, 0],
+    [100, 100],
+    [0, 100]
+]
+
+const polygon = [
+    [0, 0],
+    [100, 0]
+]
+
+const dotHF = ([x1, y1], [x2, y2]) => [x1 + (x2 - x1) / 2, y1 + (y2 - y1) / 2]
+
+const newDot = (i1, i2) => polygon.splice(i2, 0, dotHF(polygon[i1], polygon[i2]))
+
+newDot(0, 1)
+
+const newDotsBetween = () => {
+    //const index = (polygon.length / 2) | 0
+    //newDot(index, index + 1)
+    //newDot(index - 1, index)
+    //newDot(polygon.length - 2, polygon.length - 1)
+    //newDot(0, 1)
+    //console.log(index)
+    //newDot(polygon.length - 2, polygon.length - 1)
+    //newDot(polygon.length - 2, polygon.length - 1)
+
+    const poly = polygon
+    //const curve = UIBezierPath()
+
+    poly.map(([x, y], i) => (i > ((poly.length / 2) | 0) ? newDot(i - 1, i) : null))
+    //console.log(Math.floor((polygon.length - 1) / 2))
+    poly.map(([x, y], i) => (i < Math.floor((poly.length - 1) / 2) ? newDot(i, i + 1) : null))
+    //console.log(Math.floor((polygon.length - 1) / 2))
+}
+
+//newDot(0, 1)
+//newDot(0, 1)
+//newDot(2, 3)
+//newDot(3, 4)
+//newDot(0, 1)
+//newDot(0, 1)
+
+newDotsBetween()
+//newDotsBetween()
+//newDotsBetween()
+//newDotsBetween()
+//newDotsBetween()
+
+//newDotsBetween()
+
+//console.log(polygon)
+
+//const clip = [
+//    [0, 0],
+//    [100, 0],
+//    [100, 100],
+//    [0, 100],
+//]
+//    .reduce((s, [x, y], i) => {
+//        s.push([x, y])
+//        s.push([x * 0.5, y * 0.2])
+
+//        return s
+//    }, [])
+//    .map(([x, y]) => `${x}% ${y}%`)
+//    .join(', ')
+
+//const timers = ref({ mainImg: false, mainImgCount: 0 })
+//setInterval(async function () {
+//    timers.value.mainImg = !timers.value.mainImg
+//    timers.value.mainImgCount === 0 ? timers.value.mainImgCount++ : null
+//}, 5000)
+
+const toProcent = (polygon) => polygon.map(([p1, p2]) => `${p1}% ${p2}%`).join(', ')
+
+// ---------------------------------------------
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,10 @@
 <template>
     <LayoutPage pagecl="main">
-        <!--  -->
-
-        <div class="m-auto">{{ polygon }}</div>
-        <!--<div class="mt-auto mx-auto mb-4">{{ basePolygon }}</div>-->
-
-        <div class="flex mb-auto mx-auto gap-20">
-            <div
-                class="main-clip self-center bg-dark-100 h-30vh w-30vw overflow-hidden"
-                :style="`clip-path: polygon(${toProcent(polygon)})`"
-            ></div>
-
-            <div
-                class="main-clip self-center bg-dark-100 h-30vh w-30vw overflow-hidden"
-                :style="`clip-path: polygon(${toProcent(polygon_test)})`"
-            ></div>
-        </div>
-
-        <!--<div class="flex flex-grow">
-            <div :class="['relative flex-grow h-full w-full overflow-hidden']">
+        <div class="flex flex-grow">
+            <div :class="['relative flex justify-end flex-grow h-full w-full overflow-hidden']">
                 <ItemMediaImg
                     :class="[
-                        'absolute h-full w-full ml-10 object-cover object-right',
+                        'absolute h-full w-auto mt-0 -mr-[4vh] object-cover object-right'
                         //timers.mainImg
                         //    ? 'animated animate-fade-in'
                         //    : 'animated animate-fade-out',
@@ -30,10 +13,10 @@
                 ></ItemMediaImg>
             </div>
 
-            <div :class="['relative flex-grow h-full w-full overflow-hidden']">
+            <div :class="['relative flex justify-start flex-grow h-full w-full overflow-hidden']">
                 <ItemMediaImg
                     :class="[
-                        'absolute h-full w-full -ml-10 object-cover object-left',
+                        'absolute h-full w-auto mt-0 -ml-[4vh] object-cover object-left'
                         //!timers.mainImg
                         //    ? 'animated animate-fade-in'
                         //    : 'animated animate-fade-out',
@@ -44,114 +27,14 @@
                     src="main/face_2.webp"
                 ></ItemMediaImg>
             </div>
-        </div>-->
+        </div>
 
         <!--<div class="nav-bottom"></div>-->
         <!--  -->
     </LayoutPage>
 </template>
 
-<script setup>
-import { pointsOnBezierCurves } from 'points-on-curve'
-
-console.log(
-    pointsOnBezierCurves([
-        [5, 5],
-        [10, 10],
-        [15, 5],
-        [20, 0]
-    ])
-)
-
-const polygon_test = [
-    [0, 0],
-    //
-    //[5, 5],
-    //[10, 10],
-    //[15, 5],
-    //[20, 0],
-    ...pointsOnBezierCurves([
-        [5, 5],
-        [10, 10],
-        [15, 5],
-        [20, 0]
-    ]),
-    //
-    [100, 0],
-    [100, 100],
-    [0, 100]
-]
-
-const polygon = [
-    [0, 0],
-    [100, 0]
-]
-
-const dotHF = ([x1, y1], [x2, y2]) => [x1 + (x2 - x1) / 2, y1 + (y2 - y1) / 2]
-
-const newDot = (i1, i2) => polygon.splice(i2, 0, dotHF(polygon[i1], polygon[i2]))
-
-newDot(0, 1)
-
-const newDotsBetween = () => {
-    //const index = (polygon.length / 2) | 0
-    //newDot(index, index + 1)
-    //newDot(index - 1, index)
-    //newDot(polygon.length - 2, polygon.length - 1)
-    //newDot(0, 1)
-    //console.log(index)
-    //newDot(polygon.length - 2, polygon.length - 1)
-    //newDot(polygon.length - 2, polygon.length - 1)
-
-    const poly = polygon
-    //const curve = UIBezierPath()
-
-    poly.map(([x, y], i) => (i > ((poly.length / 2) | 0) ? newDot(i - 1, i) : null))
-    //console.log(Math.floor((polygon.length - 1) / 2))
-    poly.map(([x, y], i) => (i < Math.floor((poly.length - 1) / 2) ? newDot(i, i + 1) : null))
-    //console.log(Math.floor((polygon.length - 1) / 2))
-}
-
-//newDot(0, 1)
-//newDot(0, 1)
-//newDot(2, 3)
-//newDot(3, 4)
-//newDot(0, 1)
-//newDot(0, 1)
-
-newDotsBetween()
-//newDotsBetween()
-//newDotsBetween()
-//newDotsBetween()
-//newDotsBetween()
-
-//newDotsBetween()
-
-//console.log(polygon)
-
-//const clip = [
-//    [0, 0],
-//    [100, 0],
-//    [100, 100],
-//    [0, 100],
-//]
-//    .reduce((s, [x, y], i) => {
-//        s.push([x, y])
-//        s.push([x * 0.5, y * 0.2])
-
-//        return s
-//    }, [])
-//    .map(([x, y]) => `${x}% ${y}%`)
-//    .join(', ')
-
-//const timers = ref({ mainImg: false, mainImgCount: 0 })
-//setInterval(async function () {
-//    timers.value.mainImg = !timers.value.mainImg
-//    timers.value.mainImgCount === 0 ? timers.value.mainImgCount++ : null
-//}, 5000)
-
-const toProcent = (polygon) => polygon.map(([p1, p2]) => `${p1}% ${p2}%`).join(', ')
-</script>
+<script setup></script>
 
 <style lang="scss">
 .main {
