a749fa5bc65b75a45053c4240c91574f1d430f25
Maxim Semenuk <gtxyss@gmail.com>
2024-02-14 23:24:39 +0300

    bk

--- pages/go-to-[slug].vue
+++ pages/go-to-[slug].vue
@@ -7,5 +7,14 @@ const {
     params: { slug }
 } = useRoute()
 
+//await navigateTo('https://nuxt.com', {
+//    external: true,
+//    open: true
+//}).then(() => setTimeout(() => navigateTo('/'), 5000))
+
 console.log(slug)
+
+useHead({
+    title: slug
+})
 </script>
