e4d93fcddc8ae5d15c8d5ff4a57561deb606ce09
Maxim Semenuk <gtxyss@gmail.com>
2024-02-19 15:44:10 +0300

    bk

---
+++ z-mod/json_to_yaml/index.js
@@ -0,0 +1,49 @@
+﻿import { writeFileSync } from 'node:fs'
+import { parse, stringify } from 'yaml'
+//
+;(async () => {
+    const links = await import('./referrers.json', {
+        assert: {
+            type: 'json'
+        }
+    }).then((res) => res.default[0])
+
+    const rows = links.rows
+
+    rows.map((row) => {
+        const [
+            id,
+            title,
+            created_at,
+            updated_at,
+            created_by_id,
+            updated_by_id,
+            short,
+            href,
+            name,
+            description,
+            alt,
+            partnership,
+            top
+        ] = row
+
+        writeFileSync(
+            `./z-mod/json_to_yaml/.output/common/links/${short}.yaml`,
+            stringify({ link: isNull(href), name: isNull(name), alt: isNull(alt) })
+        )
+
+        if (isZero(partnership))
+            writeFileSync(
+                `./z-mod/json_to_yaml/.output/ru/referrers/${short}.yaml`,
+                stringify({ title: isNull(title), description: isNull(description) })
+            )
+    })
+})()
+
+function isNull(item) {
+    return item === 'NULL' ? null : item
+}
+
+function isZero(item) {
+    return item === '0' ? false : true
+}
