d4601477d80ab38635ff95e8dea540c23453d198
Maxim Semenuk <gtxyss@gmail.com>
2024-02-26 21:01:43 +0300

    bk

--- modules/generate-sitemap.ts
+++
@@ -1,5 +0,0 @@
-﻿import { defineNuxtModule, createResolver } from '@nuxt/kit'
-
-export default defineNuxtModule({
-    setup(options, nuxt) {}
-})
--- plugins/generate-sitemap.server.ts
+++ plugins/generate-sitemap.server.ts
@@ -1,4 +1,8 @@
-﻿import sitemap from 'sitemap'
+﻿import { SitemapStream, streamToPromise } from 'sitemap'
+import { writeFileSync } from 'fs'
+import { Readable } from 'stream'
+
+const name = 'sitemap.xml'
 
 export default defineNuxtPlugin({
     name: 'generate-sitemap',
@@ -7,22 +11,37 @@ export default defineNuxtPlugin({
     async setup(nuxtApp) {
         //const client = useSupabaseClient()
         //const localesCodes = nuxtApp.$config.locales.map(({ code }) => code)
-        //const links = useArrayUnique(
-        //    [
-        //        //...((await client.from('links').select('*')?.data) ?? []),
-        //        ...(await queryContent(`/common/links`)
-        //            .find()
-        //            .catch(() => []))
-        //    ].map((item) => `/go-to-${item?.title?.toLowerCase() || item?.slug}`)
-        //).value
+        //...((await client.from('links').select('*')?.data) ?? []),
+
+        const linksContent = useArrayUnique(
+            (
+                await queryContent(`/common/links`)
+                    .find()
+                    .catch(() => [])
+            ).map((item) => `/go-to-${item?._path?.toLowerCase()?.replace('/common/links/', '') || item?.slug}`)
+        ).value
 
         const router = useRouter()
-        const routes = router.options.routes.map(({ path }) => path).filter((path) => !path.includes(':'))
+        const routes = router.options.routes
+            .map(({ path }) => path)
+            .filter((path) => !path.includes(':') && !path.includes('admin'))
 
-        //console.log(routes)
-        console.log(sitemap)
+        const siteLinks = [...getSitemapUtm(routes, 0.5), ...getSitemapUtm(linksContent, 0.1)]
+        const stream = new SitemapStream({ hostname: 'https://maxiosigma.web.app' })
+
+        streamToPromise(Readable.from(siteLinks).pipe(stream)).then((data) => {
+            const sitemap = data.toString()
+            writeFileSync(`public/${name}`, sitemap)
+        })
+
+        //console.log(siteLinks)
+        //console.log(sitemap)
     },
     env: {
         islands: true
     }
 })
+
+function getSitemapUtm(array, priority = 0.3, changefreq = 'daily') {
+    return array.map((url) => ({ url, changefreq, priority }))
+}
