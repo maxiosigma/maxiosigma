84a0ef827e11f971a6d9a4b04390b745d17a6848
Maxim Semenuk <gtxyss@gmail.com>
2024-02-26 17:14:40 +0300

    bk

--- plugins/generate-sitemap.server.ts
+++ plugins/generate-sitemap.server.ts
@@ -20,13 +20,26 @@
 //    console.log(locales, links)
 //})
 
+import sitemap from 'sitemap'
+
 export default defineNuxtPlugin({
     name: 'generate-sitemap',
     enforce: 'pre',
     async setup(nuxtApp) {
-        //const { locales } = useI18n()
+        const client = useSupabaseClient()
         const locales = nuxtApp.vueApp.$nuxt.$i18n.locales.value.map(({ iso }) => iso.toLowerCase())
-        console.log(locales)
+
+        const links = useArrayUnique(
+            [
+                //...((await client.from('links').select('*')?.data) ?? []),
+                ...(await queryContent(`/common/links`)
+                    .find()
+                    .catch(() => []))
+            ].map((item) => `/go-to-${item?.title?.toLowerCase() || item?.slug}`)
+        ).value
+
+        //console.log(links)
+        console.log(sitemap)
     },
     hooks: {
         'app:created'() {
