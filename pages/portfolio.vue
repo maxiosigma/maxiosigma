1b595bfc0201769fbd0c9eeb03ba82fd20597163
Maxim Semenuk <gtxyss@gmail.com>
2024-02-28 02:40:48 +0300

    bk

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -16,10 +16,24 @@ const baseMeterGroups = [
     { label: 'Media', value: 24 },
     { label: 'System', value: 10 }
 ]
-
 const colorsMeterGroups = useRandTwColors(baseMeterGroups.length)
 
-const meterGroups = ref(baseMeterGroups.map((it, i) => ({ ...it, color: colorsMeterGroups[i] })))
+const meterGroups = useState(() => baseMeterGroups.map((it, i) => ({ ...it, color: colorsMeterGroups[i] })))
+
+//const payloadKey = 'works-colors'
+//if (process.server) {
+//    const baseMeterGroups = [
+//        { label: 'Apps', value: 16 },
+//        { label: 'Messages', value: 8 },
+//        { label: 'Media', value: 24 },
+//        { label: 'System', value: 10 }
+//    ]
+
+//    const colorsMeterGroups = useRandTwColors(baseMeterGroups.length)
+
+//    useNuxtApp().payload.data[payloadKey] = baseMeterGroups.map((it, i) => ({ ...it, color: colorsMeterGroups[i] }))
+//}
+//const meterGroups = useNuxtData(payloadKey).data
 
 useHead({
     title: 'Портфолио'
