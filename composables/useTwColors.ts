d732b72310af486f48015e731aecb942904ffaf1
Maxim Semenuk <gtxyss@gmail.com>
2024-02-28 01:44:11 +0300

    bk

--- composables/useTwColors.ts
+++ composables/useTwColors.ts
@@ -1,8 +1,10 @@
 ﻿import colors from 'tailwindcss/colors'
 
 export default function () {
-    return Object.values(colors)
-        .filter((it) => typeof it === typeof {})
-        .map((it) => Object.values(it))
-        .flat()
+    return colors
+        ? Object.values(colors)
+              .filter((it) => typeof it === typeof {})
+              .map((it) => Object.values(it))
+              .flat()
+        : []
 }
