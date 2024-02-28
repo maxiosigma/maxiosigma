ff14ab143658a30a6434c1a2b061b4f64acba0b4
Maxim Semenuk <gtxyss@gmail.com>
2024-02-28 03:44:12 +0300

    bk

---
+++ components/Template/Portfolio/Meter.vue
@@ -0,0 +1,49 @@
+﻿<template>
+    <PrimeMeterGroup
+        :value="meterGroups"
+        :orientation="orientation ?? 'vertical'"
+        :labelOrientation="labelOrientation ?? 'vertical'"
+    />
+</template>
+
+<script setup>
+const props = defineProps(['orientation', 'labelOrientation'])
+
+const baseMeterGroups = [
+    //
+    { label: 'Nuxt', value: 20 },
+    { label: 'Tailwind', value: 15 },
+    { label: 'Strapi CMS', value: 15 },
+    { label: 'October CMS' },
+    { label: 'Wordpress CMS' },
+    { label: 'Handlebars' },
+    { label: 'PowerPoint' },
+    { label: 'Photoshop' },
+    { label: '3DsMax' },
+    { label: 'Blender' },
+    { label: 'MatLab' },
+    { label: 'Html5' },
+    { label: 'Visio' },
+    { label: 'CSS3' },
+    { label: 'Maya' },
+    { label: 'PHP' },
+    { label: 'PUG' },
+    { label: 'GIT' },
+    { label: 'TS', value: 10 },
+    { label: 'JS' },
+    { label: 'C#' }
+]
+
+const colorsMeterGroups = useRandTwColors(baseMeterGroups.length)
+const valuesMeterGroups = baseMeterGroups.reduce((s, it) => (s += it?.value ?? 0) && s, 0)
+
+const meterGroups = useState(
+    () =>
+        baseMeterGroups.map((it, i) => ({
+            color: colorsMeterGroups[i],
+            value: 100 / valuesMeterGroups,
+            ...it
+        }))
+    //.sort((a, b) => b.value - a.value)
+)
+</script>
--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -1,49 +1,13 @@
 ﻿<template>
     <LayoutPage>
         <div class="flex self-end py-8 pr-8 justify-content-center text-slate-300">
-            <PrimeMeterGroup :value="meterGroups" orientation="vertical" labelOrientation="vertical" />
+            <TemplatePortfolioMeter />
         </div>
     </LayoutPage>
 </template>
 
 <script setup>
 const works = []
-
-const baseMeterGroups = [
-    //
-    { label: 'Nuxt', value: 20 },
-    { label: 'Strapi', value: 15 },
-    { label: 'Tailwind', value: 15 },
-    { label: 'October CMS' },
-    { label: 'Wordpress CMS' },
-    { label: 'Handlebars' },
-    { label: 'PowerPoint' },
-    { label: 'Photoshop' },
-    { label: 'Blender' },
-    { label: 'MatLab' },
-    { label: 'Html5' },
-    { label: 'Visio' },
-    { label: 'CSS3' },
-    { label: 'PHP' },
-    { label: 'PUG' },
-    { label: 'GIT' },
-    { label: 'TS', value: 10 },
-    { label: 'JS' },
-    { label: 'C#' }
-]
-
-const colorsMeterGroups = useRandTwColors(baseMeterGroups.length)
-const valuesMeterGroups = baseMeterGroups.reduce((s, it) => (s += it?.value ?? 0) && s, 0)
-
-const meterGroups = useState(
-    () =>
-        baseMeterGroups.map((it, i) => ({
-            color: colorsMeterGroups[i],
-            value: 100 / valuesMeterGroups,
-            ...it
-        }))
-    //.sort((a, b) => b.value - a.value)
-)
 
 useHead({
     title: 'Портфолио'
