9bc6021ce17bd767e50fcfa7c88284160ea76995
Maxim Semenuk <gtxyss@gmail.com>
2024-03-01 22:09:57 +0300

    bk

--- composables/useAllVariantsArrayItems.ts
+++ composables/useAllVariantsArrayItems.ts
@@ -1,10 +1,10 @@
 ﻿export default function (items: any[]) {
     const length = items.length
 
-    //console.log(useRange(length - 1, 2))
+    console.log(useRange(length - 1, 2).map((it) => useRange(it)))
     //console.log(items[0])
 
-    console.log(permutator([1, 2, 3, 4, 5]))
+    //console.log(permutator([1, 2, 3, 4, 5]))
 
     //return items
 }
