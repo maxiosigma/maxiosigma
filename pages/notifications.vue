07c56cedda1760220b99a87078a1c9a9b4da83f7
Maxim Semenuk <gtxyss@gmail.com>
2024-03-18 00:45:25 +0300

    bk

--- pages/notifications.vue
+++ pages/notifications.vue
@@ -2,24 +2,24 @@
     <LayoutPage :title="title">
         <div class="flex flex-wrap container gap-10 self-center justify-center py-10">
             <div
-                class="grid grid-cols-2 p-5 border-4 border-self-4"
+                class="grid grid-cols-3 auto-cols-auto p-5 border-4 border-self-4"
                 v-for="(notification, ni) in notifications"
                 :key="ni"
             >
                 <div>Key:</div>
-                <div>{{ notification?.key }}</div>
+                <div class="col-span-2">{{ notification?.key }}</div>
 
                 <div>Path:</div>
-                <div>{{ notification.value?.url }}</div>
+                <div class="col-span-2">{{ notification.value?.url }}</div>
 
                 <div>Date:</div>
-                <div>{{ notification.value?.date }}</div>
+                <div class="col-span-2">{{ notification.value?.date }}</div>
 
                 <div>Page:</div>
-                <div>{{ notification.value?.page }}</div>
+                <div class="col-span-2">{{ notification.value?.page }}</div>
 
                 <div>Body:</div>
-                <div>{{ notification.value?.body }}</div>
+                <div class="col-span-2">{{ notification.value?.body }}</div>
             </div>
         </div>
     </LayoutPage>
