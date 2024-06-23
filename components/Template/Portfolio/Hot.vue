cced4e977f3591b36f005ff53a2f3520c764562a
Maxim Semenuk <gtxyss@gmail.com>
2024-06-23 04:29:23 +0300

    bk

--- components/Template/Portfolio/Hot.vue
+++ components/Template/Portfolio/Hot.vue
@@ -6,7 +6,7 @@
             class=":uno: flex text-gray-400 rounded-2xl gap-4 overflow-hidden"
             :class="[$style['multi-border'], wi !== 1 ? ':uno: absolute !hidden' : '']"
         >
-            <div class="min-w-1/4 max-w-1/4 pl-0.5 py-0.5" :class="[$style['multi-border']]">
+            <div class="min-w-3/8 max-w-3/8 pl-0.5 py-0.5" :class="[$style['multi-border']]">
                 <ItemMediaImg
                     class="flex-shrink-1 w-full h-full rounded-l-2xl"
                     :class="[$style['multi-border']]"
@@ -14,10 +14,11 @@
                 />
             </div>
 
-            <div class="flex flex-col py-4 pr-4 self-center flex-grow gap-2">
+            <div class="flex flex-col py-4 pr-4 self-center flex-grow gap-4">
                 <!--<div>{{ work }}</div>-->
                 <div class="text-3xl text-self-4">{{ work.title }}</div>
                 <div class="">{{ work.description }}</div>
+                <PrimeButton class="self-start">Посмотреть →</PrimeButton>
             </div>
             <!--{{ work?.images?.[0] }}-->
         </div>
